b0f2babd0b1758995c11721397423ae508006bb8
Giuliano Serawop <gserawop13@gmail.com>
2021-05-04 16:59:32 -0600

    updating question in index.js, inquirer questions on adding roles

--- db/schema.sql
+++ db/schema.sql
@@ -3,29 +3,25 @@ CREATE database employee_tracker;
 
 USE employee_tracker;
 
-CREATE TABLE department {
-    id INT PRIMARY KEY,
-    department_name VARCHAR(30),
-    PRIMARY KEY
-};
+CREATE TABLE department (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    department_name VARCHAR(30)
+);
 
-CREATE TABLE employee_role {
-    id INT PRIMARY KEY,
+CREATE TABLE employee_role (
+    id INT AUTO_INCREMENT PRIMARY KEY,
     title VARCHAR(30),
     salary DECIMAL ,
-    department_id INT 
-    PRIMARY KEY,
-}
+    department_id INT, 
+    FOREIGN KEY (department_id) REFERENCES department(id)
+);
 
-CREATE TABLE employee {
-    id INT PRIMARY KEY,
+CREATE TABLE employee (
+    id INT AUTO_INCREMENT PRIMARY KEY,
     first_name VARCHAR(30),
     last_name VARCHAR(30),
     role_id INT,
     manager_id INT,
-    PRIMARY KEY,
-}
+    FOREIGN KEY (role_id) REFERENCES employee_role(id)
+);
 
-SELECT * FROM department
-SELECT * FROM employee_role
-SELECT * FROM employee
--- index.js
+++ index.js
@@ -1,91 +1,218 @@
+const mysql = require("mysql");
 const fs = require('fs')
 const inquirer = require('inquirer')
 
+const connection = mysql.createConnection({
+  host: "localhost",
+  port: 3306,
+  user: "root",
+  password: "Lucaserawop",
+  database: "employee_tracker",
+});
 
-let questions = [
-  {
-    type: "list",
-    name: "addDepartment",
-    message: "What department would you like to add?",
-    choices: [],
-  },
-  {
-    type: "list",
-    name: "addRoles",
-    message: "What role would you like to add?",
-    choices: [],
-  },
-  {
-    type: "list",
-    name: "addEmployees",
-    message: "Who is the new employee?",
-    choices: [],
-  },
-  {
-    type: "list",
-    name: "viewDepartments",
-    message: "What department would you like to view?",
-    choices: [],
-  },
-  {
-    type: "list",
-    name: "viewRole",
-    message: "What role would you like to view?",
-    choices: [],
-  },
-  {
-    type: "list",
-    name: "viewEmployee",
-    message: "What employee would you like to view?",
-    choices: [],
-  },
-  {
-    type: "list",
-    name: "updateEmployeeRole",
-    message: "What employee role would you like to update?",
-    choices: [],
-  },
-  {
-    type: "list",
-    name: "updateEmployeeManager",
-    message: "What employee manager would you like to update?",
-    choices: [],
-  },
-  {
-    type: "list",
-    name: "viewEmployeeManager",
-    message: "What manager would you like to view?",
-    choices: [],
-  },
-  {
-    type: "list",
-    name: "deleteDepartment",
-    message: "What department would you like to delete?",
-    choices: [""],
-  },
-  {
-    type: "list",
-    name: "deleteRole",
-    message: "What role would you like to delete?",
-    choices: [""],
-  },
-  {
-    type: "list",
-    name: "deleteEmploylee",
-    message: "What employlee would you like to delete?",
-    choices: [""],
-  },
-  {
-    type: "list",
-    name: "viewUtiliezedBudget",
-    message: "This is your utilized budget.",
-    choices: [""],
-  },
-  {
-    type: "list",
-    name: "totalBudget",
-    message: "This is your total budget.",
-    choices: [""],
-  },
-];
+connection.connect((err) => {
+  if (err) throw err;
+  runSearch();
+});
 
+const runSearch = () => {
+  inquirer
+    .prompt({
+      name: "action",
+      type: "list",
+      message: "What would you like to do?",
+      choices: [
+        "Add departments",
+        "Add roles",
+        "Add employees",
+        "View departments",
+        "View employees",
+        "View roles",
+        "Update employee role",
+      ],
+    })
+    .then((answer) => {
+      switch (answer.action) {
+        case "Add departments":
+          addDepartment()
+          break;
+
+        case "Add roles":
+          addRoles()
+          break;
+
+        case "Add employees":
+          addEmployees()
+          break;
+
+        case "View departments":
+          veiwDepartment()
+          break;
+
+        case "View employees":
+          viewEmployee()
+          break;
+
+        case "View roles":
+          viewRoles()
+          break;
+
+        case "Update employee role":
+          updateEmployeeRole()
+          break;
+
+        default:
+          console.log(`Invalid action: ${answer.action}`);
+          break;
+      }
+    });
+};
+
+const addDepartment = () => {
+  inquirer
+    .prompt({
+      name: "addDepartment",
+      type: "input",
+      message: "What department would you like to add?",
+    })
+    .then((answer) => {
+      const query = connection.query(
+        "INSERT INTO department SET ?",
+        {
+          department_name: answer.addDepartment,
+        },
+        (err, res) => {
+          if (err) throw err;
+          console.log(`${res.affectedRows} product inserted!\n`)
+        }
+      );
+
+  // logs the actual query being run
+  console.log(query.sql);
+  });
+};
+
+const addRoles = () => {
+  inquirer
+    .prompt({
+      name: "addRoles",
+      type: "input",
+      message: "What roles would you like to add?",
+    })
+    .then((answer) => {
+      const query = connection.query(
+        "INSERT INTO department SET ?",
+        {
+          department_name: answer.addDepartment,
+        },
+        (err, res) => {
+          if (err) throw err;
+          console.log(`${res.affectedRows} product inserted!\n`);
+        }
+      );
+
+      // logs the actual query being run
+      console.log(query.sql);
+    });
+};
+
+const addEmployees = () => {
+  inquirer
+    .prompt({
+      name: "artist",
+      type: "input",
+      message: "What artist would you like to search for?",
+    })
+    .then((answer) => {
+      const query = "SELECT position, song, year FROM top5000 WHERE ?";
+      connection.query(query, { artist: answer.artist }, (err, res) => {
+        res.forEach(({ position, song, year }) => {
+          console.log(
+            `Position: ${position} || Song: ${song} || Year: ${year}`
+          );
+        });
+        runSearch();
+      });
+    });
+};
+
+
+const veiwDepartment = () => {
+  inquirer
+    .prompt({
+      name: "artist",
+      type: "input",
+      message: "What artist would you like to search for?",
+    })
+    .then((answer) => {
+      const query = "SELECT position, song, year FROM top5000 WHERE ?";
+      connection.query(query, { artist: answer.artist }, (err, res) => {
+        res.forEach(({ position, song, year }) => {
+          console.log(
+            `Position: ${position} || Song: ${song} || Year: ${year}`
+          );
+        });
+        runSearch();
+      });
+    });
+};
+
+const viewRoles = () => {
+  inquirer
+    .prompt({
+      name: "artist",
+      type: "input",
+      message: "What artist would you like to search for?",
+    })
+    .then((answer) => {
+      const query = "SELECT position, song, year FROM top5000 WHERE ?";
+      connection.query(query, { artist: answer.artist }, (err, res) => {
+        res.forEach(({ position, song, year }) => {
+          console.log(
+            `Position: ${position} || Song: ${song} || Year: ${year}`
+          );
+        });
+        runSearch();
+      });
+    });
+};
+
+const viewEmployee = () => {
+  inquirer
+    .prompt({
+      name: "artist",
+      type: "input",
+      message: "What artist would you like to search for?",
+    })
+    .then((answer) => {
+      const query = "SELECT position, song, year FROM top5000 WHERE ?";
+      connection.query(query, { artist: answer.artist }, (err, res) => {
+        res.forEach(({ position, song, year }) => {
+          console.log(
+            `Position: ${position} || Song: ${song} || Year: ${year}`
+          );
+        });
+        runSearch();
+      });
+    });
+};
+
+const updateEmployeeRole = () => {
+  inquirer
+    .prompt({
+      name: "artist",
+      type: "input",
+      message: "What artist would you like to search for?",
+    })
+    .then((answer) => {
+      const query = "SELECT position, song, year FROM top5000 WHERE ?";
+      connection.query(query, { artist: answer.artist }, (err, res) => {
+        res.forEach(({ position, song, year }) => {
+          console.log(
+            `Position: ${position} || Song: ${song} || Year: ${year}`
+          );
+        });
+        runSearch();
+      });
+    });
+};
